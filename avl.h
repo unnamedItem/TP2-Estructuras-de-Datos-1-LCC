415c9aa17de508aeac97ef6cc5b80fbb3d8ae527
Franco Cusmai <cusmaifranco@hotmail.com>
2020-06-01 00:27:43 -0300

    Prueba

---
+++ avl.h
@@ -0,0 +1,56 @@
+#ifndef AVL_H
+#define AVL_H
+
+typedef struct _Intervalo {
+  double a, b; // [a, b] intervalo
+  double extremo; // extremo maximo b
+} Intervalo;
+
+typedef struct _AVL {
+  Intervalo *dato;
+  int FB; /* factor de balance*/
+  struct _AVL *izq, *der;
+} AVL
+
+//==============================================================================
+
+//Comprueba si dos intervalos se intersectan
+int interscta (Intervalo *intervalo1, Intervalo *intervalo2);
+
+//Reserva memoria para un intervalo
+Intervalo* crear_intervalo (double a, double b);
+
+//Crear arbol
+AVL* itree_crear ();
+
+//Insertar en el arbol
+void itree_insertar (Intervalo* intervalo, AVL* arbol);
+
+//Liberar el arbol
+void itree_destruir (AVL* arbol);
+
+//Eliminar nodo del arbol
+AVL* itree_eliminar (Intervalo* intervalo, AVL* arbol);
+
+//Recorrido en profundidad del arbol
+void itree_recorrer_dfs (AVL* arbol);
+
+//Recorrido a lo ancho del arbol
+void itree_recorrer_bfs (AVL* arbol);
+
+//Busqueda de interseccion de intervalos en el arbol
+Intervalo* itree_intersectar (Intervalo* intervalo, AVL* arbol);
+
+//Rotacion simple derecha
+void rotacion_simpleR (AVL* arbol);
+
+//Rotacion simple izquierda
+void rotacion_simpleL (AVL* arbol);
+
+//Rotacion doble derecha
+void rotacion_dobleR (AVL* arbol);
+
+//Rotacion doble izquierda
+void rotacion_dobleL (AVL* arbol);
+
+#endif
---
+++ main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "avl.h"
+
+//==============================================================================
+/*
+Recibe el nombre de una operacion y retorna un numero correspondiente a la
+misma.
+*/
+int opcion (char* operacion) {
+  if (!strcmp(operacion, "i")) return 1; /* 1: Imprimir */
+  if (!strcmp(operacion, "e")) return 2; /* 2: Eliminar */
+  if (!strcmp(operacion, "?")) return 3; /* 3: Intersectar */
+  if (!strcmp(operacion, "dfs")) return 4; /* 4: dfs */
+  if (!strcmp(operacion, "bfs")) return 5; /* 5: bfs */
+  if (!strcmp(operacion, "salir")) return 0; /* 0: salir */
+  return -1; /* operacion no valida */
+}
+
+//==============================================================================
+
+int main() {
+
+  char operacion[8];
+  double a, b;
+  int op = -1;
+
+  AVL* arbol = itree_crear (); // inicializa el arbol
+
+  /* Formato de entrada "opcion [a, b]" */
+  while (op != 0) {
+    if(scanf("%s [%lf, %lf]", operacion, &a, &b) != 3) return 0; // read error
+    op = opcion (operacion);
+
+    if (a > b) return 0; // formato de intervalo invalido
+
+    Intervalo* intervalo = crear_intervalo (a, b); // crea el intervalo [a, b]
+
+    /* interprete */
+    switch (op) {
+      // Insertar
+      case 1:
+        printf("Insertar\n");
+        itree_insertar (arbol);
+      break;
+      // Elimnar
+      case 2:
+        printf("Elimnar\n");
+        itree_eliminar (intervalo, arbol);
+        free(intervalo);
+      break;
+      // Intersectar
+      case 3:
+        printf("Intersectar\n");
+        itree_intersectar (intervalo, arbol);
+        free(intervalo);
+      break;
+      // DFS
+      case 4:
+        printf("dfs\n");
+        itree_recorrer_dfs (arbol);
+        free(intervalo);
+      break;
+      // BFS
+      case 5:
+        printf("bfs\n");
+        itree_recorrer_bfs (arbol);
+        free(intervalo);
+      break;
+      // Salir
+      case 0:
+        printf("Adios\n");
+        itree_destruir (arbol);
+        free(intervalo);
+      // Operacion invalida
+      default:
+        printf("Operacion no valida\n");
+      break;
+    }
+  }
+
+  return 0;
+}
