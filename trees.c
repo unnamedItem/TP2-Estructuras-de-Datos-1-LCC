8e3a14821106b5d236a3560a74763dedd31d820e
lautarojbc <lautarojbc@gmail.com>
2020-06-02 02:02:50 -0300

    agrego tree.c

--- trees.c
+++ trees.c
@@ -10,6 +10,14 @@ Intervalo* crear_intervalo (double a, double b) {
     return nuevoIntervalo;
 };
 
+//creo que esto anda, no estoy 100 por ciento seguro
+int intersecta (Intervalo *intervalo1, Intervalo *intervalo2) {
+   if(intervalo1->a <= intervalo2->b && intervalo2->a <= intervalo1->b) {
+      return 1;
+   }
+   else return 0;
+}
+
 AVL* itree_crear () {
     return NULL;
 }
