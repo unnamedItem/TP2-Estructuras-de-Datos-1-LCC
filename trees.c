9da400442f7e2b89399b69de08fb5496f011ffcf
Franco Cusmai <cusmaifranco@hotmail.com>
2020-06-05 15:07:05 -0300

    add <actualizar_rotacion_izquierda> y <actualizar_rotacion_derecha>

--- trees.c
+++ trees.c
@@ -36,6 +36,40 @@ void itree_recorrer_dfs (AVL *arbol) {
   itree_recorrer_dfs (arbol->der);
 }
 
+void actualizar_extremo (AVL* nodo1, AVL* nodo2) {
+  if (nodo1->extremo < nodo2->extremo)
+    nodo1->extremo = nodo2->extremo;
+}
+
+void actualizar_rotacion_derecha (AVL* nodo1, AVL* nodo2 ) {
+  if (nodo2->extremo <= nodo1->extremo) {
+    nodo2->extremo = nodo2->dato->b; //Nodo 1 no posee el maximo del sub arbol
+    if (nodo2->der != NULL) {
+      actualizar_extremo (nodo2, nodo2->der);
+      if (nodo2->der->extremo == nodo1->extremo)
+        return; //Todos los nodos son correctos
+    actualizar_extremo (nodo2, nodo2->izq);
+    return; // Nodo 1 posee el maximo del sub arbol
+    }
+  actualizar_extremo (nodo1, nodo2); // Nodo 2 posee el maximo
+  }
+}
+
+void actualizar_rotacion_izquierda (AVL* nodo1, AVL* nodo2) {
+  if (nodo2->extremo <= nodo1->extremo) {
+    nodo2->extremo = nodo2->dato->b; //Nodo 1 no posee el maximo del sub arbol
+    if (nodo2->izq != NULL) {
+      actualizar_extremo (nodo2, nodo2->izq);
+      if (nodo2->izq->extremo == nodo1->extremo)
+        return; //Todos los nodos son correctos
+    actualizar_extremo (nodo2, nodo2->der);
+    return; // Nodo 1 posee el maximo del sub arbol
+    }
+  actualizar_extremo (nodo1, nodo2); // Nodo 2 posee el maximo
+  }
+}
+
+
 void rotacion_simpleL (AVL* arbol) {
   AVL* aux = arbol->izq->der;
   arbol->izq->der = arbol;
@@ -44,6 +78,7 @@ void rotacion_simpleL (AVL* arbol) {
   arbol->izq = aux;
   arbol->FB = 0;
   arbol = aux2;
+  actualizar_rotacion_izquierda (arbol, arbol->izq);
 }
 
 void rotacion_simpleR (AVL* arbol) {
@@ -54,15 +89,18 @@ void rotacion_simpleR (AVL* arbol) {
   arbol->der = aux;
   arbol->FB = 0;
   arbol = aux2;
+  actualizar_rotacion_derecha (arbol, arbol->der);
 }
 
 void rotacion_dobleL (AVL* arbol) {
   rotacion_simpleR (arbol->izq);
+  actualizar_extremo (arbol, arbol->izq);
   rotacion_simpleL (arbol);
 }
 
 void rotacion_dobleR (AVL* arbol) {
   rotacion_simpleL (arbol->der);
+  actualizar_extremo (arbol, arbol->der);
   rotacion_simpleR (arbol);
 }
 
@@ -74,19 +112,20 @@ void insertar (AVL* arbol, int* aumento, Intervalo* intervalo) {
     arbol->der = NULL;
     arbol->FB = 0;
     arbol->extremo = intervalo->b;
-    aumento = 1;
+    *aumento = 1;
   } else {
     if (intervalo->a < arbol->dato->a) {
       insertar (arbol->izq, aumento, intervalo);
+      actualizar_extremo (arbol, arbol->izq);
       if (aumento) {
         switch (arbol->FB) {
           case -1:
             arbol->FB = 0;
-            aumento = 0;
+            *aumento = 0;
           break;
           case 0:
             arbol->FB = 1;
-            aumento = 1;
+            *aumento = 1;
           break;
           case 1:
             if (arbol->izq->FB == 1) {
@@ -94,11 +133,13 @@ void insertar (AVL* arbol, int* aumento, Intervalo* intervalo) {
             } else {
               rotacion_dobleL (arbol);
             }
+            *aumento = 1;
           break;
         }
       }
     } else {
       insertar (arbol->der, aumento, intervalo);
+      actualizar_extremo (arbol, arbol->der);
       if (aumento) {
         switch (arbol->FB) {
           case -1:
@@ -107,14 +148,15 @@ void insertar (AVL* arbol, int* aumento, Intervalo* intervalo) {
             } else {
               rotacion_simpleR (arbol);
             }
+            *aumento = 1;
           break;
           case 0:
             arbol->FB = -1;
-            aumento = 1;
+            *aumento = 1;
           break;
           case 1:
             arbol->FB = 0;
-            aumento = 0;
+            *aumento = 0;
           break;
         }
       }
@@ -123,6 +165,6 @@ void insertar (AVL* arbol, int* aumento, Intervalo* intervalo) {
 }
 
 void itree_insertar (AVL* arbol, Intervalo* intervalo) {
-  int aumento;
+  int* aumento;
   insertar (arbol, aumento, intervalo);
 }
